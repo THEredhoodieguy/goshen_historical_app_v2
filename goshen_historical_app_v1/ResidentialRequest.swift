4b12b63cc2678a5858af751546e33b48a4ac3517
Matthew Pletcher <matthewwp@goshen.edu>
2016-03-23 11:22:51 -0400

    Added files from Coder branch

---
+++ goshen_historical_app_v1/ResidentialRequest.swift
@@ -0,0 +1,87 @@
+
+//
+//  ResidentialRequest.swift
+//  goshen_historical_app_v1
+//
+//  Created by Matthew Wesley Pletcher on 3/11/16.
+//  Copyright © 2016 Matthew Pletcher. All rights reserved.
+//
+
+import Foundation
+
+import Alamofire
+
+
+func getResidential(url_string: String) -> Array<Array<String>> {
+	//Currently uses general read from file function, write_to_file() not yet implemented
+    
+    var stuff_string=""
+    
+    var arr = Array<Array<String>>()
+	
+	Alamofire.request(.GET, url_string)
+		.responseString { response in
+			stuff_string = response.result.value!
+			
+			if (stuff_string.rangeOfString("404  Not Found") != nil) {
+				//Use contingency string
+                print("Page 404'd")
+                
+                //If get request does not return a valid page, read from the file
+                stuff_string = read_from_file()
+			}
+            else {
+                
+                print("Page didn't 404")
+                
+                //If get request does return a valid page, write the latest version of the page to file
+                write_to_file(stuff_string)
+            }
+				values_array = stuff_string.characters.split { $0 == "\n"}.map(String.init)
+			
+			for i in values_array {
+				arr.append(i.componentsSeparatedByString("|"))
+			}
+			
+	}
+    
+    return(arr)
+}
+
+
+func getBusiness(url_string: String) -> Array<Array<String>> {
+    //DOES NOT HAVE BUSINESS SPECIFIC ADDRESS IMPLEMENTED
+    //PLEASE IMPLEMENT BEFORE USING
+    
+    var stuff_string=""
+    
+    var arr = Array<Array<String>>()
+    
+    Alamofire.request(.GET, url_string)
+        .responseString { response in
+            stuff_string = response.result.value!
+            
+            if (stuff_string.rangeOfString("404  Not Found") != nil) {
+                //Use contingency string
+                print("Page 404'd")
+                
+                //If get request does not return a valid page, read from the file
+                stuff_string = read_from_file()
+            }
+            else {
+                
+                print("Page didn't 404")
+                
+                //If get request does return a valid page, write the latest version of the page to file
+                write_to_file(stuff_string)
+            }
+            values_array = stuff_string.characters.split { $0 == "\n"}.map(String.init)
+            
+            for i in values_array {
+                arr.append(i.componentsSeparatedByString("|"))
+            }
+            
+    }
+    
+    return(arr)
+}
---
+++ goshen_historical_app_v1/file_handling.swift
@@ -0,0 +1,83 @@
+//
+//  file_handling.swift
+//  goshen_historical_app_v1
+//
+//  Created by Matthew Wesley Pletcher on 3/15/16.
+//  Copyright © 2016 Matthew Pletcher. All rights reserved.
+//
+
+import Foundation
+
+func read_from_file() -> String {
+    var output_str = ""
+    
+    
+    //Seems to be correct for compiled apps, but doesn't work in the simulator, leaving out for now
+    let destinationPath: String! = NSHomeDirectory() + "output_test.txt"
+    
+    //String to use for now
+    //let destinationPath: String! = "/Users/matthewwp/Desktop/goshen_historical_app_v2/goshen_historical_app_v1/output_test.txt"
+	
+    print(destinationPath)
+    
+    let filemgr = NSFileManager.defaultManager()
+	
+	
+	
+	// We need just to get the documents folder url
+	let documentsUrl =  NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+	
+	// now lets get the directory contents (including folders)
+	do {
+		let directoryContents = try NSFileManager.defaultManager().contentsOfDirectoryAtURL(documentsUrl, includingPropertiesForKeys: nil, options: NSDirectoryEnumerationOptions())
+		print(directoryContents)
+		
+	} catch let error as NSError {
+		print(error.localizedDescription)
+	}
+	
+	
+	
+    if filemgr.fileExistsAtPath(destinationPath) {
+        print("File exists")
+        do {
+            //Set file to output
+            output_str = try String(contentsOfFile: destinationPath, encoding: NSUTF8StringEncoding)
+        } catch let error as NSError {
+            print("Error: \(error)")
+        }
+        
+    }
+    
+    else {
+        print("File does not exist")
+    }
+	print("")
+	print(output_str)
+	print("")
+    return(output_str)
+    
+}
+
+func write_to_file(input_str: String) -> Void {
+	let text = input_str
+	
+	let path = NSHomeDirectory() + "text.txt"
+		
+	//writing
+	do {
+		try text.writeToFile(path, atomically: false, encoding: NSUTF8StringEncoding)
+	}
+	catch {
+		/* error handling here */
+	}
+}
+
+func file_exists() -> Bool {
+	let filemgr = NSFileManager.defaultManager()
+	let destinationPath: String! = NSHomeDirectory() + "text.txt"
+	
+	print("A file exists here")
+	
+	return(filemgr.fileExistsAtPath(destinationPath))
+}
